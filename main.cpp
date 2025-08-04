65651a1562ad22f2d03b499214c12b88aae7edd5
GeneralTV <146952172+GeneralTV@users.noreply.github.com>
2025-08-04 14:29:23 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,39 +1,59 @@
-#include <windows.h>
-#include <wininet.h>
-#include <shlwapi.h>
+#include <Windows.h>
+#include <string>
+#include <fstream>
+#include <filesystem>
+#include <plugin.h>
 
-#pragma comment(lib, "wininet.lib")
-#pragma comment(lib, "shlwapi.lib")
-#pragma comment(lib, "urlmon.lib")  // Добавлено для URLDownloadToFileA
+namespace fs = std::filesystem;
 
-const char* GITHUB_INDEX = "https://raw.githubusercontent.com/GeneralTV/Teest/main/index.html";
-const char* GITHUB_JS = "https://raw.githubusercontent.com/GeneralTV/Teest/main/custom.js";
-const char* LOCAL_INDEX = "uiresources\\index.html";
-const char* LOCAL_JS = "uiresources\\assets\\custom.js";
+class ResourceLoader {
+public:
+    ResourceLoader() {
+        // Пути к файлам внутри папки с плагином
+        fs::path pluginDir = GetPluginDir();
+        fs::path indexSrc = pluginDir / "uiresources" / "index.html";
+        fs::path customJsSrc = pluginDir / "uiresources" / "assets" / "custom.js";
 
-void CreateDirectoryRecursive(const char* path) {
-    char dir[MAX_PATH];
-    strcpy_s(dir, MAX_PATH, path);
-    for (char* p = strchr(dir + 1, '\\'); p; p = strchr(p + 1, '\\')) {
-        *p = '\0';
-        CreateDirectoryA(dir, NULL);
-        *p = '\\';
+        // Пути назначения в папке игры
+        fs::path gameDir = GetGameDir();
+        fs::path indexDst = gameDir / "uiresources" / "index.html";
+        fs::path customJsDst = gameDir / "uiresources" / "assets" / "custom.js";
+
+        // Создаем папки, если их нет
+        fs::create_directories(indexDst.parent_path());
+        fs::create_directories(customJsDst.parent_path());
+
+        // Копируем файлы
+        bool success = true;
+        success &= CopyFile(indexSrc, indexDst);
+        success &= CopyFile(customJsSrc, customJsDst);
+
+        if (success) {
+            Debug::Log("[ResourceLoader] Файлы успешно скопированы!");
+        } else {
+            Debug::Log("[ResourceLoader] Ошибка копирования файлов!");
+        }
     }
-}
 
-void DownloadFile(const char* url, const char* path) {
-    CreateDirectoryRecursive(path);
-    URLDownloadToFileA(NULL, url, path, 0, NULL);
-}
-
-extern "C" __declspec(dllexport) void ScriptMain() {
-    DownloadFile(GITHUB_INDEX, LOCAL_INDEX);
-    DownloadFile(GITHUB_JS, LOCAL_JS);
-}
-
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved) {
-    if (reason == DLL_PROCESS_ATTACH) {
-        ScriptMain();
+private:
+    fs::path GetPluginDir() {
+        char path[MAX_PATH];
+        GetModuleFileNameA(GetModuleHandle(NULL), path, MAX_PATH);
+        return fs::path(path).parent_path();
     }
-    return TRUE;
-}
+
+    fs::path GetGameDir() {
+        char path[MAX_PATH];
+        GetCurrentDirectoryA(MAX_PATH, path);
+        return fs::path(path);
+    }
+
+    bool CopyFile(const fs::path& src, const fs::path& dst) {
+        try {
+            fs::copy_file(src, dst, fs::copy_options::overwrite_existing);
+            return true;
+        } catch (...) {
+            return false;
+        }
+    }
+} loader;
